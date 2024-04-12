4e08aace62f088eaa915dda6692a99ac2b4fa649
Tascu_Daniel <tascudaniel22@stud.ase.ro>
2024-04-12 15:56:46 +0300

    Add project files.

---
+++ ContContabilitate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROIECT_PAW
+{
+    public class ContContabilitate
+    {
+        private int ContId;
+        private string NumeCont;
+        private string TipCont;
+    
+        public ContContabilitate(int contId, string numeCont, string tipCont)
+        {
+            ContId = contId;
+            NumeCont = numeCont;
+            TipCont = tipCont;
+        }
+
+        public int ID
+        {  get { return ContId; } 
+            set { ContId = value; }
+        }
+
+        public string Nume
+        {
+            get { return NumeCont; }
+            set { NumeCont = value; }
+        }
+
+        public string Tip
+        {
+            get { return TipCont; }
+            set {  TipCont = value; }
+        }
+
+    } //END OF CLASS
+}
---
+++ OperatiiContabile.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROIECT_PAW
+{
+    public class OperatiiContabile 
+    {
+        private ContContabilitate listaCredit;//lista 
+        private ContContabilitate listaDebit;//lista
+        //FIX T 
+        //sume ca n T 
+        //emulare de T 
+        //metoda pentru calculare 
+        //DATA !!
+        //T si sa adauga sau sa editez, adaugare operatie
+        //totaluri
+        private float soldInitial;
+        private float soldFinal;//far astea 
+  
+        public OperatiiContabile(ContContabilitate credit, ContContabilitate debit, float SoldInitial, float SoldFinal)
+        {
+            listaCredit = credit;
+            listaDebit = debit;
+            soldInitial = SoldInitial;
+            soldFinal = SoldFinal;
+        }
+
+        public ContContabilitate ContCredit
+        {
+            get { return listaCredit; }
+            set
+            {
+                listaCredit = value;
+            }
+        }
+
+
+            public ContContabilitate ContDebit
+        {
+            get { return listaDebit; }
+            set { listaDebit = value;}
+        }
+
+        public float SoldInitial
+        {
+            get { return SoldInitial; }
+               set { SoldInitial = value;}
+        }
+
+        public float SoldFinal
+        {
+            get { return SoldFinal; }
+            set { SoldFinal = value;}
+        }
+    }//END OF CLASS
+}
